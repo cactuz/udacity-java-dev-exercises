aaa3b7a585f482cd70c5ec2f9b7641aa14ae03c1
rrdiez <rudy.r.diez@aexp.com>
2019-08-31 22:00:25 -0400

    update rest demo add auth

--- microservices/rest-data-demo/readme.md
+++ microservices/rest-data-demo/readme.md
@@ -3,11 +3,22 @@ This is a demo of the API using Spring Boot Data Rest.  This has even more magic
 
 This project is paired with a eureka-demo project so that this microservice will be registered.
 
-#Pre-requisites:
-Open up the eureka-demo projet and run it, before this one.
+Finally, the service is also secured with basic authentication.
+
+#Optional Pre-requisites:
+(Optional)
+If Eureka service registry is desired, set to `eureka.client.enabled` to `true` .Then before 
+running this server, open and run the eureka-demo project before 
+this one.
+
+
+#Authentication
+Basic Auth: 
+Username: admin
+Password: password
 
 #What to when this runs:
-1. Try `http://localhost:8080`
-2. Try `http://localhost:8080/dogs` - which should provide you all the data from data.sql. 
-3. Try POST `http://localhost:8080/dogs`.
-4. If you completed the pre-requisite before running this server, you should be able to see the 'DOG-SERVICE' registed in the eureka server `http://localhost:8761`
+1. Try `http://localhost:8762`
+2. Try `http://localhost:8762/dogs` - which should provide you all the data from data.sql. 
+3. Try POST `http://localhost:8762/dogs`.
+4. (Optional, see pre-requisite)You should be able to see the 'DOG-SERVICE' registed in the eureka server `http://localhost:8761`
--- microservices/rest-data-demo/src/test/java/com/aexp/exercises/udacity/dogapidemo/DogApiDemoApplicationTests.java
+++ microservices/rest-data-demo/src/test/java/com/aexp/exercises/udacity/dogapidemo/DogApiDemoApplicationTests.java
@@ -3,17 +3,23 @@ package com.aexp.exercises.udacity.dogapidemo;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -30,22 +36,33 @@ import static org.hamcrest.CoreMatchers.equalTo;
 @SpringBootTest
 public class DogApiDemoApplicationTests {
 
+	private String uri = "http://localhost:8762/dogs";
+	private static CredentialsProvider provider;
+	private static HttpClient client;
+
 	@BeforeClass
 	public static void setupApi() {
+
 		DogApiDemoApplication.main(new String[]{});
+
+		provider = new BasicCredentialsProvider();
+		UsernamePasswordCredentials credentials
+				= new UsernamePasswordCredentials("admin", "password");
+		provider.setCredentials(AuthScope.ANY, credentials);
+
+		client = HttpClientBuilder.create()
+				.setDefaultCredentialsProvider(provider)
+				.build();
 	}
 
 	@Test
 	public void getDogs() throws Exception{
-		HttpUriRequest request = new HttpGet( "http://localhost:8080/dogs");
-		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-
+		HttpResponse httpResponse = client.execute(new HttpGet(uri));
 		Assert.assertThat(
 				httpResponse.getStatusLine().getStatusCode(),
 				equalTo(HttpStatus.SC_OK));
 
 		String responseBody = EntityUtils.toString(httpResponse.getEntity());
-
 		Assert.assertTrue(responseBody.contains("Aspin"));
 		Assert.assertTrue(responseBody.contains("Golden Retriever"));
 
@@ -54,24 +71,18 @@ public class DogApiDemoApplicationTests {
 	@Test
 	public void postDog() throws Exception{
 
-		CloseableHttpClient client = HttpClients.createDefault();
-		HttpPost httpPost = new HttpPost( "http://localhost:8080/dogs");
-
-		String json = "{\"name\": \"rocket\",\"breed\":\"Shitzu\",\"origin\" : \"Japan\", \"id\" " +
-				": \"1\"}";
+		HttpPost httpPost = new HttpPost(uri);
+		String json = "{\"name\": \"rocket\",\"breed\":\"Shitzu\",\"origin\" : \"Japan\", \"id\": \"1\"}";
 		StringEntity entity = new StringEntity(json);
 		httpPost.setHeader("Content-Type", "application/json");
 		httpPost.setEntity(entity);
 		HttpResponse postResponse = client.execute(httpPost);
-
 		Assert.assertThat(
 				postResponse.getStatusLine().getStatusCode(),
 				equalTo(HttpStatus.SC_CREATED));
 
-		HttpUriRequest httpGet = new HttpGet( "http://localhost:8080/dogs");
-		HttpResponse getResponse = HttpClientBuilder.create().build().execute(httpGet);
+		HttpResponse getResponse = client.execute(new HttpGet(uri));
 		String responseBody = EntityUtils.toString(getResponse.getEntity());
-
 		Assert.assertTrue(responseBody.contains("rocket"));
 	}
 }
